fd782e9df355953a2c152f739ec1c3c0bbcc92a3
gcmillora <gregmillora.dev@gmail.com>
2025-02-27 15:20:51 +0800

    add: payments library - create checkout session

---
+++ libs/payments/src/payments.service.ts
@@ -0,0 +1,68 @@
+import { UsersLibService } from '@app/users';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class PaymentsLibService {
+  private readonly apiKey: string;
+  private readonly storeId: string;
+  private readonly apiUrl = 'https://api.lemonsqueezy.com/v1/checkouts';
+
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly configService: ConfigService,
+    private readonly userLibService: UsersLibService,
+  ) {
+    this.apiKey = this.configService.get('LEMON_SQUEEZY_API_KEY');
+    this.storeId = this.configService.get('LEMON_SQUEEZY_STORE_ID');
+  }
+
+  async createCheckoutSession() {
+    /* This is a sample request to create a checkout session.
+     * Change the appropriate values to create a checkout session for a user.
+     */
+    const response = await fetch(this.apiUrl, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/vnd.api+json',
+        'Content-Type': 'application/vnd.api+json',
+        Authorization: `Bearer ${this.apiKey}`,
+      },
+      body: JSON.stringify({
+        data: {
+          type: 'checkouts',
+          attributes: {
+            checkout_data: {
+              custom: {
+                userId: 'sample',
+                email: 'sample@sample.com',
+              },
+            },
+            product_options: {
+              redirect_url: `https://google.com`,
+            },
+          },
+          relationships: {
+            store: {
+              data: {
+                type: 'stores',
+                id: this.storeId,
+              },
+            },
+            variant: {
+              data: {
+                type: 'variants',
+                id: '1',
+              },
+            },
+          },
+        },
+      }),
+    });
+
+    const jsonResponse = await response.json();
+
+    return jsonResponse;
+  }
+}
